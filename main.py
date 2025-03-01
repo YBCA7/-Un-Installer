7d43e1f1ea293faa9c8f2ada5afea67ef4220bfd
Aviation Engineer <109074663+YBCA7@users.noreply.github.com>
2025-03-01 22:25:33 +0800

    Add English translation

--- main.py
+++ main.py
@@ -5,32 +5,32 @@ from sys import prefix
 from threading import Thread
 
 r = Tk()
-r.title('(Un)installer')
-Label(text="需要装卸的包: ").grid(row=0, column=0)
+r.title('-Un-installer')
+Label(text="需要装卸的包  Name of Package: ").grid(row=0, column=0)
 e = Entry(width=50)
 e.grid(row=0, column=1)
 path = prefix + "\\Scripts\\pip.exe"
 
-insb = Button(text="安装", width=61)
+insb = Button(text="安装  Install", width=61)
 insb.grid(row=1, columnspan=2)
-unib = Button(text="卸载 （请在弹出的窗口中输入y）", width=61)
+unib = Button(text="卸载（在弹出窗口中输y）  Uninstall", width=61)
 unib.grid(row=2, columnspan=2)
 
 
 def inst():
-    insb.config(state="disabled", text="执行中…")
+    insb.config(state="disabled", text="执行中  Executing…")
     unib.config(state="disabled")
     system(path + " install " + e.get())
-    insb.config(state="normal", text="安装")
+    insb.config(state="normal", text="安装  Install")
     unib.config(state="normal")
 
 
 def uninst():
     insb.config(state="disabled")
-    unib.config(state="disabled", text="执行中…")
+    unib.config(state="disabled", text="执行中  Executing…")
     system(path + " uninstall " + e.get())
     insb.config(state="normal")
-    unib.config(state="normal", text="卸载 （请在弹出的窗口中输入Y）")
+    unib.config(state="normal", text="卸载（请在弹出窗口中输Y）  Uninstall")
 
 
 def install():
